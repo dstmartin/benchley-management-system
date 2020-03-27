37cf2268df6bdc5b77b0acb40329d7c7d2a8a07e
Donald St. Martin <dstmartin@dsa.reldom.tamu.edu>
2020-03-26 21:31:20 -0500

    Style updates

--- resources/views/livewire/people.blade.php
+++ resources/views/livewire/people.blade.php
@@ -1,29 +1,28 @@
 <div>
     <label for="search">Search</label>
-    <input id="search" type="text" wire:model="search" placeholder="Search directory by name, email, or phone number." />
-    <p>{{ $search }}</p>
+    <input id="search" type="text" wire:model="search" placeholder="Search directory by name, email, or phone number." class="border px-2 py-1" />
 
     <div id="directory">
-        <div>
+        <div class="mb-3">
             Per Page &nbsp;
-            <select wire:model="count" name="" id="count">
+            <select wire:model="count" name="" id="count" class="border">
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="15">15</option>
                 <option value="25">25</option>
             </select>
         </div>
-        <table class="table-auto">
-            <thead>
+        <table class="table-auto w-full table-striped">
+            <thead class="bg-gray-800 text-white">
                 <tr>
                     <th class="border px-4 py-2">
-                        <a wire:click.prevent="sortBy('firstname')" role="button" href="#">
+                        <a wire:click.prevent="sortBy('firstname')" role="button" href="#" class="text-white">
                             Name
                             @include('partials.sort-icon', ['field' => 'firstname'])
                         </a>
                     </th>
                     <th class="border px-4 py-2">
-                        <a wire:click.prevent="sortBy('email')" role="button" href="#">
+                        <a wire:click.prevent="sortBy('email')" role="button" href="#" class="text-white">
                             Email
                             @include('partials.sort-icon', ['field' => 'email'])
                         </a>
@@ -31,13 +30,13 @@
                     <th class="border px-4 py-2">Phone Number(s)</th>
                     <th class="border px-4 py-2">Birthday</th>
                     <th class="border px-4 py-2">
-                        <a wire:click.prevent="sortBy('gender')" role="button" href="#">
+                        <a wire:click.prevent="sortBy('gender')" role="button" href="#" class="text-white">
                             Gender
                             @include('partials.sort-icon', ['field' => 'gender'])
                         </a>
                     </th>
                     <th class="border px-4 py-2">
-                        <a wire:click.prevent="sortBy('is_member')" role="button" href="#">
+                        <a wire:click.prevent="sortBy('is_member')" role="button" href="#" class="text-white">
                             Member
                             @include('partials.sort-icon', ['field' => ''])
                         </a>
